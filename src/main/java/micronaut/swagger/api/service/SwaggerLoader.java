16c1ac841ebc27d21fe1b404ed18906f281d6632
Anton Kurako <goodforgod.dev@gmail.com>
2020-09-21 01:14:01 +0300

    YamlMerger and SwaggerLoader added

--- src/main/java/micronaut/swagger/api/service/SwaggerLoader.java
+++ src/main/java/micronaut/swagger/api/service/SwaggerLoader.java
@@ -1,10 +1,86 @@
 package micronaut.swagger.api.service;
 
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.CollectionUtils;
+import micronaut.swagger.api.config.SwaggerConfig;
+import micronaut.swagger.api.model.Swagger;
+import org.yaml.snakeyaml.Yaml;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 /**
  * Description in progress
  *
  * @author Anton Kurako (GoodforGod)
  * @since 20.9.2020
  */
+@Requires(beans = SwaggerConfig.class)
+@Singleton
 public class SwaggerLoader {
+
+    private static final String SWAGGER_DIR = "META-INF/swagger";
+    private static final String SWAGGER_MERGED = "swagger-merged.yml";
+
+    private Collection<Swagger> cachedSwaggers = null;
+    private Swagger merged = null;
+
+    private final YamlMerger yamlMerger;
+
+    @Inject
+    public SwaggerLoader(YamlMerger yamlMerger) {
+        this.yamlMerger = yamlMerger;
+    }
+
+    public Optional<Swagger> getServiceSwagger() {
+        return getSwaggers().stream().min(Comparator.comparingLong(Swagger::getCreated));
+    }
+
+    public Optional<Swagger> getMergedSwagger() {
+        if(merged != null)
+            return Optional.of(merged);
+
+        final Collection<Swagger> swaggers = getSwaggers();
+        final Map<Object, Object> mergedYaml = yamlMerger.merge(swaggers);
+        if(CollectionUtils.isEmpty(mergedYaml))
+            return Optional.empty();
+
+        try (final Writer writer = new BufferedWriter(new FileWriter(SWAGGER_MERGED))) {
+            new Yaml().dump(mergedYaml, writer);
+            this.merged = new Swagger(new File(SWAGGER_MERGED).toURI(), Instant.now().getEpochSecond());
+            return Optional.of(merged);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    public Collection<Swagger> getSwaggers() {
+        if (cachedSwaggers != null)
+            return cachedSwaggers;
+
+        try (final JarFile jarFile = new JarFile(SWAGGER_DIR)) {
+            final Enumeration<JarEntry> entries = jarFile.entries();
+            final List<Swagger> swaggers = new ArrayList<>();
+
+            while (entries.hasMoreElements()) {
+                final JarEntry entry = entries.nextElement();
+                if (entry.getName().endsWith(".yml") || entry.getName().endsWith(".yaml")) {
+                    swaggers.add(new Swagger(new URI(entry.getName()), entry.getCreationTime().to(TimeUnit.SECONDS)));
+                }
+            }
+
+            this.cachedSwaggers = swaggers;
+            return swaggers;
+        } catch (IOException | URISyntaxException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/micronaut/swagger/api/service/YamlMerger.java
@@ -0,0 +1,99 @@
+package micronaut.swagger.api.service;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.CollectionUtils;
+import micronaut.swagger.api.config.SwaggerConfig;
+import micronaut.swagger.api.model.Swagger;
+import org.yaml.snakeyaml.Yaml;
+
+import javax.inject.Singleton;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Description in progress
+ *
+ * @author Anton Kurako (GoodforGod)
+ * @since 21.9.2020
+ */
+@Requires(beans = SwaggerConfig.class)
+@Singleton
+public class YamlMerger {
+
+    public Map<Object, Object> merge(Collection<Swagger> swaggers) {
+        final Map<Object, Object> result = new LinkedHashMap<>();
+
+        swaggers.stream()
+                .map(this::swaggerAsMap)
+                .forEach(yaml -> merge(result, yaml));
+
+        return result;
+    }
+
+    private Map<Object, Object> swaggerAsMap(Swagger swagger) {
+        return new Yaml().load(getClass().getResourceAsStream(swagger.getUri().getPath()));
+    }
+
+    @SuppressWarnings("unchecked")
+    private void merge(Map<Object, Object> merged, Map<Object, Object> yaml) {
+        if (CollectionUtils.isEmpty(yaml))
+            return;
+
+        for (Map.Entry<Object, Object> entry : yaml.entrySet()) {
+            final Object v = entry.getValue();
+            final Object key = entry.getKey();
+            if (v == null) {
+                merged.put(key, null);
+                continue;
+            }
+
+            final Object existingValue = merged.get(key);
+            if (existingValue != null) {
+                if (v instanceof Map) {
+                    if (existingValue instanceof Map) {
+                        merge((Map<Object, Object>) existingValue, (Map<Object, Object>) v);
+                    } else if (existingValue instanceof String
+                            || existingValue instanceof Boolean
+                            || existingValue instanceof Double
+                            || existingValue instanceof Integer) {
+                        throw new IllegalArgumentException(
+                                "Cannot merge complex element into a simple element: " + key);
+                    } else {
+                        throwUnknownValueType(key, v);
+                    }
+                } else if (v instanceof List) {
+                    final Object value = merged.get(key);
+                    final boolean v2 = value instanceof List;
+                    if (!v2)
+                        throw new IllegalArgumentException("Cannot merge a list with a non-list: " + key);
+
+                    ((List<Object>) value).addAll(((List<Object>) v));
+                } else if (v instanceof String
+                        || v instanceof Boolean
+                        || v instanceof Double
+                        || v instanceof Integer) {
+                    merged.put(key, v);
+                } else {
+                    throwUnknownValueType(key, v);
+                }
+            } else {
+                if (v instanceof Map
+                        || v instanceof List
+                        || v instanceof String
+                        || v instanceof Boolean
+                        || v instanceof Integer
+                        || v instanceof Double) {
+                    merged.put(key, v);
+                } else {
+                    throwUnknownValueType(key, v);
+                }
+            }
+        }
+    }
+
+    private void throwUnknownValueType(Object key, Object yamlValue) {
+        throw new IllegalArgumentException("Cannot merge element of unknown type: " + key + ": " + yamlValue.getClass().getSimpleName());
+    }
+}
