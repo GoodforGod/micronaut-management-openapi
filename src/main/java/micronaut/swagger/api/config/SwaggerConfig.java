c61d8882b87776d884e82b683749198f78224093
Anton Kurako <goodforgod.dev@gmail.com>
2020-09-21 01:13:50 +0300

    SwaggerConfig enriched build.gradle updated application-test.yml config updated Swagger model added

--- src/main/java/micronaut/swagger/api/config/SwaggerConfig.java
+++ src/main/java/micronaut/swagger/api/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package micronaut.swagger.api.config;
 
+import io.micronaut.context.annotation.ConfigurationBuilder;
 import io.micronaut.context.annotation.ConfigurationProperties;
 import io.micronaut.context.annotation.Requires;
 import micronaut.swagger.api.SwaggerSettings;
+
+import java.util.List;
 
 /**
  * @author Anton Kurako (GoodforGod)
@@ -13,8 +16,79 @@ import micronaut.swagger.api.SwaggerSettings;
 public class SwaggerConfig {
 
     public class SwaggerUIConfig {
+
+        private String path;
         private boolean enabled;
+
+        public String getPath() {
+            return path;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
     }
 
     private boolean enabled;
+    private boolean merge;
+    private String path;
+    private List<String> exclude;
+
+    @ConfigurationBuilder(prefixes = "set", configurationPrefix = "ui")
+    private final SwaggerUIConfig uiConfig = new SwaggerUIConfig();
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isMerge() {
+        return merge;
+    }
+
+    public void setMerge(boolean merge) {
+        this.merge = merge;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public List<String> getExclude() {
+        return exclude;
+    }
+
+    public void setExclude(List<String> exclude) {
+        this.exclude = exclude;
+    }
+
+    public SwaggerUIConfig getUiConfig() {
+        return uiConfig;
+    }
+
+    @Override
+    public String toString() {
+        return "SwaggerConfig{" +
+                "enabled=" + enabled +
+                ", merge=" + merge +
+                ", path='" + path + '\'' +
+                ", exclude=" + exclude +
+                ", uiConfig=" + uiConfig +
+                '}';
+    }
 }
---
+++ src/main/java/micronaut/swagger/api/model/Swagger.java
@@ -0,0 +1,51 @@
+package micronaut.swagger.api.model;
+
+import java.net.URI;
+import java.nio.file.attribute.FileTime;
+import java.util.Objects;
+
+/**
+ * Description in progress
+ *
+ * @author Anton Kurako (GoodforGod)
+ * @since 21.9.2020
+ */
+public class Swagger {
+
+    private final URI uri;
+    private final long created;
+
+    public Swagger(URI uri, long created) {
+        this.uri = uri;
+        this.created = created;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Swagger swagger = (Swagger) o;
+        return Objects.equals(uri, swagger.uri);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uri);
+    }
+
+    @Override
+    public String toString() {
+        return "Swagger{" +
+                "uri=" + uri +
+                ", created=" + created +
+                '}';
+    }
+}
